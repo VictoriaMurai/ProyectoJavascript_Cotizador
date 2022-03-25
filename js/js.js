d5e2a780613d103c35c1967354bb661e5c5c4890
Victoria Murai <murai.victoria@gmail.com>
2022-03-24 23:40:57 -0300

    Segunda entrega del proyecto final

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+/*
+
 class ModeloDeVehiculo {
     constructor(modelo, precioBase, anio) {
         this.modelo = modelo
@@ -13,7 +15,7 @@ class ModeloDeVehiculo {
     }
 }
 
-class NuevoCotizador {
+class Cotizador {
     constructor(vehiculoSelec, seguroSelec, diasSelec){
 
         this.vehiculo = vehiculoSelec
@@ -79,7 +81,7 @@ function cotizarAlquilerAuto(){
     const vehiculo = parseInt(selectAuto.value)
     const seguro = parseInt(selectSeguro.value)
     const dias = parseInt(inputDias.value)
-    const costoAlquiler = new NuevoCotizador (vehiculo, seguro, dias)
+    const costoAlquiler = new Cotizador (vehiculo, seguro, dias)
     const res = costoAlquiler.calcularAlquiler()
 
     alert("El costo del alquiler es: ARS ", res)
@@ -107,7 +109,7 @@ const auto5 = new ModeloDeVehiculo ("Renault Kadjar", 17800, 2017)
 const selectAuto = document.querySelector("#selectAuto")
 const selectSeguro = document.querySelector("#selectSeguro")
 const inputDias = document.querySelector("#inputDias")
-const btnCotizar = document.querySelector("#submit")
+const btnCotizar = document.querySelector("#btnCotizar")
 const btnCancelar = document.querySelector("#cancelar")
 
 const suCotizacion = document.getElementById("suCotizacion")
@@ -125,3 +127,120 @@ const cargarResultado = () => {
             resultadoCotizacion.innerText = "Su cotización es de ARS", cotizarAlquilerAuto().res
     }
 }
+
+*/
+
+
+
+const selectAuto = document.querySelector("#selectAuto")
+const selectSeguro = document.querySelector("#selectSeguro")
+const inputDias = document.querySelector("#inputDias")
+
+const btnCotizar = document.querySelector("#btnCotizar")
+const btnCancelar = document.querySelector("#cancelar")
+const valorAlquiler = document.querySelector("#valorAlquiler")
+
+
+const arrayModeloDeVehiculo = [{"modelo": "Audi A3 Sportback", "precio": 10500, "anio": 2020}, 
+                            {"modelo": "Citroen C4 Picasso", "precio": 11000, "anio": 2015},
+                            {"modelo": "Mercedes B Class", "precio": 12500, "anio": 2021},
+                            {"modelo": "Opel Zafira", "precio": 15400, "anio": 2018},
+                            {"modelo": "Renault Kadjar", "precio": 17800, "anio": 2017}]
+
+
+const arraySeguro = [{"seguro": "Seguro base", "precio": 2500},
+                    {"seguro": "Seguro integral", "precio": 4000}]
+
+
+
+class Cotizador {
+    constructor(vehiculoSelec, seguroSelec, diasSelec){
+        this.vehiculo = vehiculoSelec
+        this.seguro = seguroSelec
+        this.dias = diasSelec
+    }
+
+    calcularAlquiler = () => {
+        const resultado = (this.vehiculo + this.seguro) * dias
+            return parseFloat(resultado)
+    }
+}
+
+const cargarModeloDeVehiculo = () => {
+    let optionMDV
+        for (let elemento of arrayModeloDeVehiculo) {
+            optionMDV += `<option value= "${elemento.precio}">${elemento.modelo}</option>`
+        }
+        return optionMDV
+}
+
+const cargarSeguro = () => {
+    let optionSeg
+        for (let elemento of arraySeguro) {
+            optionSeg += `<option value= "${elemento.precio}">${elemento.seguro}</option>`
+        }
+        return optionSeg
+}
+
+const datosNoIngresados = () => {
+    return (isNaN(inputDias.value) || selectAuto.value == "" || selectSeguro.value == "")
+}
+
+const cotizarAlquiler = () => {
+    //debugger
+    if (datosNoIngresados()) {
+        alert("Por favor, complete todos los campos para cotizar.")
+        return
+    }
+    else{
+        factorVehiculo = parseFloat(selectAuto.value)
+        factorSeguro = parseFloat(selectSeguro.value)
+        dias = parseInt(inputDias.value)
+        valorDeAlquiler = Cotizacion.calcularAlquiler()
+        valorAlquiler.innerText = `$ ${valorDeAlquiler.toFixed(2)}`
+    }
+}
+
+const Cotizacion = new Cotizador(arrayModeloDeVehiculo, arraySeguro, dias)
+
+selectAuto.innerHTML += cargarModeloDeVehiculo()
+selectSeguro.innerHTML += cargarSeguro()
+
+btnCotizar.addEventListener("click", cotizarAlquiler)
+
+
+
+//Storage
+
+const guardarDatosJSON = () => {
+    const datosForm = {modelo: "", precio: 0}
+        datosForm.modelo = selectAuto.value
+        datosForm.precio = selectSeguro.value
+        localStorage.setItem("datosDelForm", JSON.stringify(datosForm))
+        console.info("Se ha almacenado el array en LS.")
+}
+
+btnCotizar.addEventListener("click", guardarDatosJSON)
+
+
+const guardoDatos = () => {
+    localStorage.setItem("modelo", selectAuto.value)
+    localStorage.setItem("precio", selectSeguro.value)
+}
+
+const recuperoDatos = () => {
+    selectAuto.value = localStorage.getItem("modelo")
+    selectSeguro.value = localStorage.getItem("precio")
+}
+
+const limparLS = () => {
+    const resp = confirm("¿Desea eliminar todos los datos almacenados?")
+        if (resp) {
+            localStorage.clear()
+            console.log("Se han eliminado todos los datos.")
+        }
+}
+
+btnCotizar.addEventListener("click", () => guardoDatos())
+
+recuperoDatos()
